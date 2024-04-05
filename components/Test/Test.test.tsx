c745385b8fb0366a2eab3f3c57bf934ee2905f61
OotaShikou <ootashikou@gmail.com>
2024-04-06 03:36:45 +0900

    init

--- components/Test/Test.test.tsx
+++ components/Test/Test.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+
 import { Test } from '.'
 
 test('renders Test component', () => {
-  render(<Test />)
+  render(<Test data={undefined} />)
 })
--- components/Test/index.tsx
+++ components/Test/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 
-type Props = {}
+type Props = {
+  data: unknown
+}
 
 export const Test: FC<Props> = () => {
   return <div>Test</div>
