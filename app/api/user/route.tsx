4ef8f735d5b85d693583cfeb676228eac401900c
OotaShikou <ootashikou@gmail.com>
2024-05-01 11:46:59 +0900

    不要なファイルを削除

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -4,10 +4,7 @@ import { authenticateToken } from '@/lib/authenticateToken'
 
 export async function GET(req: NextRequest) {
   const result = await authenticateToken(req)
-
-  if ('error' in result && result.error) {
-    return NextResponse.json({ error: `Invalid: ${result.error}` })
-  }
+  if ('error' in result && result.error) return NextResponse.json({ error: `Invalid: ${result.error}` })
 
   // 認証が成功した場合の処理
   const user = result
